2c1642a1fb8c01af1aa84a977319ccc5fe69e875
Mario Milošević <mariomilosevic887@gmail.com>
2023-10-24 13:06:01 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,56 @@
+// varijable
+const kockica1 = document.querySelector(".kockica1");
+const kockica2 = document.querySelector(".kockica2");
+const reset = document.querySelector(".reset");
+let totalScorePlayer1 = document.querySelector(".totalScorePlayer1");
+let totalScorePlayer2 = document.querySelector(".totalScorePlayer2");
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+//////////////////////////
+let player1dice, player2dice;
+let player1Score = 0,
+  player2Score = 0;
+// Lista
+const lista = [kockica1, kockica2, totalScorePlayer1, totalScorePlayer2];
+
+// funkcije
+
+function rollTheDice(dice, playerScore) {
+  let randomNumber = Math.floor(Math.random() * 6) + 1;
+  dice.innerHTML = randomNumber;
+  playerScore += randomNumber;
+  return playerScore;
+}
+
+button1.addEventListener("click", function () {
+  player1Score = rollTheDice(kockica1, player1Score);
+  totalScorePlayer1.textContent = player1Score;
+  console.log(`player1score ${player1Score}`);
+  console.log(`totalScorePlayer1 ${totalScorePlayer1}`);
+  console.log(
+    `totalScoorePlayer1 text content ${totalScorePlayer1.textContent}`
+  );
+});
+
+button2.addEventListener("click", function () {
+  player2Score = rollTheDice(kockica2, player2Score);
+  totalScorePlayer2.textContent = player2Score;
+});
+
+reset.addEventListener("click", function () {
+  totalScorePlayer1.textContent =
+  totalScorePlayer2.textContent =
+    totalScorePlayer2 =
+    player1Score =
+    player2Score =
+    kockica1.textContent =
+    kockica2.textContent =
+      0;
+});
+
+function newGame(){
+const player1Name = prompt('Insert username')
+const player2Name = prompt("Insert player2 name")
+}
+
+newGame()
